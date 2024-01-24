b5dcdaee03dc92b7020dde5d5c6c241d6c76686d
Ivan-Milichnikov <john.1105@inbox.ru>
2024-01-24 17:01:59 +0300

    ggoo

--- challenges/1 - backend/data/web_app/main/views.py
+++ challenges/1 - backend/data/web_app/main/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
--- challenges/1 - backend/data/web_app/web_app/settings.py
+++ challenges/1 - backend/data/web_app/web_app/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'news',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "main/static",
